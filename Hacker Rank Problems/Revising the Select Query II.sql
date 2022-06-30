76d15265ffd80bd04af59d3dbc49a6210aca6289
Andre Alvarez <andre.alvarez@ucsp.edu.pe>
2022-06-30 08:41:35 -0700

    Create Revising the Select Query II.sql

---
+++ Hacker Rank Problems/Revising the Select Query II.sql
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+SELECT name from city where population > 120000 AND CountryCode = "USA"

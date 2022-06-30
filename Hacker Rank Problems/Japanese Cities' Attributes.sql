efc9727f686773d4aab56d875572923ca07c5355
Andre Alvarez <andre.alvarez@ucsp.edu.pe>
2022-06-30 08:55:10 -0700

    Create Japanese Cities' Attributes.sql

---
+++ Hacker Rank Problems/Japanese Cities' Attributes.sql
@@ -0,0 +1,3 @@
+# https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+SELECT * FROM city WHERE countrycode = "JPN"
